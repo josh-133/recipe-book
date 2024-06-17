5a117e0c6e5d6feb491268f1a9ff3a6a7bf74372
Joshua Burge <joshuaburge507@gmail.com>
2024-06-17 06:42:36 +0000

    Feature: forgot to add read-recipe, update-recipe, and favourite-recipe components... oops. They are added now.

---
+++ recipe-book/src/app/pages/recipe/favourite-recipe/favourite-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favourite-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './favourite-recipe.component.html',
+  styleUrl: './favourite-recipe.component.css'
+})
+export class FavouriteRecipeComponent {
+
+}
---
+++ recipe-book/src/app/pages/recipe/read-recipe/read-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './read-recipe.component.html',
+  styleUrl: './read-recipe.component.css'
+})
+export class ReadRecipeComponent {
+
+}
