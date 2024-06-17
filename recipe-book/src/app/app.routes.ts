b5c5649517db88fb2b94b3d4d82260acb733f490
Joshua Burge <joshuaburge507@gmail.com>
2024-06-17 06:25:16 +0000

    Feature: added routes to home page and create recipe page.
    Also created a simple form for creating a recipe.

--- recipe-book/src/app/app.routes.ts
+++ recipe-book/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateRecipeComponent } from './pages/recipe/create-recipe/create-recipe.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home Page',
+    },
+    {
+        path: 'create-recipe',
+        component: CreateRecipeComponent,
+        title: 'Create Recipe'
+    }
+];
