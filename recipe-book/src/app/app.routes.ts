03d638c8249cb8c2acaf2509a7e06282130f0844
Joshua Burge <joshuaburge507@gmail.com>
2024-06-17 06:45:09 +0000

    Feature: added routing for the new components.

--- recipe-book/src/app/app.routes.ts
+++ recipe-book/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateRecipeComponent } from './pages/recipe/create-recipe/create-recipe.component';
+import { ReadRecipeComponent } from './pages/recipe/read-recipe/read-recipe.component';
+import { UpdateRecipeComponent } from './pages/recipe/update-recipe/update-recipe.component';
+import { FavouriteRecipeComponent } from './pages/recipe/favourite-recipe/favourite-recipe.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +15,20 @@ export const routes: Routes = [
         path: 'create-recipe',
         component: CreateRecipeComponent,
         title: 'Create Recipe'
+    },
+    {
+        path: 'read-recipe',
+        component: ReadRecipeComponent,
+        title: 'Read Recipe'
+    },
+    {
+        path: 'update-recipe',
+        component: UpdateRecipeComponent,
+        title: 'Update Recipe'
+    },
+    {
+        path: 'favourite-recipe',
+        component: FavouriteRecipeComponent,
+        title: 'Favourite Recipe'
     }
 ];
