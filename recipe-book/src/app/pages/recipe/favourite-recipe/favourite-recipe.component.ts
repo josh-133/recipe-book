0cc2a015d7fa86434a4234fe8a66378817fd856a
Joshua Burge <joshuaburge507@gmail.com>
2024-06-24 05:58:58 +0000

    Feature: Read recipe page now shows a specific recipe based on the id provided in the url.

--- recipe-book/src/app/pages/recipe/favourite-recipe/favourite-recipe.component.ts
+++ recipe-book/src/app/pages/recipe/favourite-recipe/favourite-recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Recipe } from '../../../interfaces/recipe';
 import { RecipeService } from '../../../services/recipe.service';
 import { CommonModule } from '@angular/common';
