e80e24f55a21ce0ad61be2be0fe03b08a7086103
Joshua Burge <joshuaburge507@gmail.com>
2024-06-24 05:15:57 +0000

    Feature: Favourites page now shows a list of all recipes that are a favourite.

--- recipe-book/src/app/pages/home/home.component.ts
+++ recipe-book/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../../interfaces/recipe';
+import { RecipeService } from '../../services/recipe.service';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faStar} from '@fortawesome/free-solid-svg-icons'
@@ -16,90 +17,10 @@ export class HomeComponent {
 
   @Input() recipe!: Recipe;
 
-  recipes: Recipe[] = [
-    {
-      id: 1,
-      title: 'Recipe 1',
-      ingredients: '1x egg',
-      method: '1. Crack egg.',
-      favourite: false
-    },
-    {
-      id: 2,
-      title: 'Recipe 2',
-      ingredients: '2x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 3,
-      title: 'Recipe 3',
-      ingredients: '3x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 4,
-      title: 'Recipe 4',
-      ingredients: '4x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 5,
-      title: 'Recipe 5',
-      ingredients: '5x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 6,
-      title: 'Recipe 6',
-      ingredients: '6x eggs',
-      method: '1. Crack eggs.',
-      favourite: false
-    },
-    {
-      id: 7,
-      title: 'Recipe 7',
-      ingredients: '7x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 8,
-      title: 'Recipe 8',
-      ingredients: '8x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 9,
-      title: 'Recipe 9',
-      ingredients: '9x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 10,
-      title: 'Recipe 10',
-      ingredients: '10x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 11,
-      title: 'Recipe 11',
-      ingredients: '11x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-    {
-      id: 12,
-      title: 'Recipe 12',
-      ingredients: '12x eggs',
-      method: '1. Crack eggs.',
-      favourite: true
-    },
-  ]
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) {
+    this.recipes = this.recipeService.getAllRecipes();
+  }
+
 }
--- recipe-book/src/app/services/recipe.service.ts
+++ recipe-book/src/app/services/recipe.service.ts
@@ -8,14 +8,99 @@ export class RecipeService {
 
   constructor() { }
 
-  recipeList: Recipe[] = []
+  recipes: Recipe[] = [
+    {
+      id: 1,
+      title: 'Recipe 1',
+      ingredients: '1x egg',
+      method: '1. Crack egg.',
+      favourite: false
+    },
+    {
+      id: 2,
+      title: 'Recipe 2',
+      ingredients: '2x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 3,
+      title: 'Recipe 3',
+      ingredients: '3x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 4,
+      title: 'Recipe 4',
+      ingredients: '4x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 5,
+      title: 'Recipe 5',
+      ingredients: '5x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 6,
+      title: 'Recipe 6',
+      ingredients: '6x eggs',
+      method: '1. Crack eggs.',
+      favourite: false
+    },
+    {
+      id: 7,
+      title: 'Recipe 7',
+      ingredients: '7x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 8,
+      title: 'Recipe 8',
+      ingredients: '8x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 9,
+      title: 'Recipe 9',
+      ingredients: '9x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 10,
+      title: 'Recipe 10',
+      ingredients: '10x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 11,
+      title: 'Recipe 11',
+      ingredients: '11x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+    {
+      id: 12,
+      title: 'Recipe 12',
+      ingredients: '12x eggs',
+      method: '1. Crack eggs.',
+      favourite: true
+    },
+  ]
 
   getAllRecipes(): Recipe[] {
-    return this.recipeList;
+    return this.recipes;
   }
 
   getRecipeById(id: number): Recipe | undefined {
-    return this.recipeList.find(recipe => recipe.id === id);
+    return this.recipes.find(recipe => recipe.id === id);
   }
 
   createRecipe(title: string, ingredients: string, method: string) {
