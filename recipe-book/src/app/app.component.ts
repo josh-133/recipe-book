fe65f32a95df8cfd7418f6f73442d86f3a9c9d6f
Joshua Burge <joshuaburge507@gmail.com>
2024-07-26 05:39:32 +0000

    Feature:  A bootstrap navbar is now added and home page has a sentence explaining what the app is.

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from '../app/pages/home/home.component'
 import { ReadRecipeComponent } from './pages/recipe/read-recipe/read-recipe.component';
 import { UpdateRecipeComponent } from './pages/recipe/update-recipe/update-recipe.component';
 import { FavouriteRecipeComponent } from './pages/recipe/favourite-recipe/favourite-recipe.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ReadRecipeComponent, UpdateRecipeComponent, FavouriteRecipeComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, ReadRecipeComponent, UpdateRecipeComponent, FavouriteRecipeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
